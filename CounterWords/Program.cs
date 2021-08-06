11319f350d4e1362f938d280a13a35b94e0fcf3a
Eduardo Sausurin <eduardo.sausurin@gmail.com>
2021-08-06 08:07:31 -0300

    Agregar archivos de proyecto.

---
+++ CounterWords/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CounterWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Este es un texto de prueba, pero texto no quiero hacerlo, porque el texto me aburro!!!";
+
+            string[] textSplited = text.Split(" ");
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            foreach (string word in textSplited)
+            {
+                string wordNormalized = normalizeWord(word);
+                if (dictionary.ContainsKey(normalizeWord(wordNormalized)))
+                {
+                    int valueActs = dictionary[wordNormalized];
+                    dictionary.Remove(wordNormalized);
+                    dictionary.Add(wordNormalized,valueActs+1);
+                }
+                else
+                {
+                    dictionary.Add(word, 1);
+                }                
+            }
+
+            foreach(string keys in dictionary.Keys)
+            {
+                Console.WriteLine("{0} : {1}", keys, dictionary[keys]);
+            }
+        }
+        private static string normalizeWord(string data)
+        {
+            return data.Replace("/[.,!]/", "");
+        }
+    }
+}
